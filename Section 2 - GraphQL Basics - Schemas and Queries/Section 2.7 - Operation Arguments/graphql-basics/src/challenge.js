4fc878b1930b87f32f98b5ead55a477c25b530bf
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-01-18 14:39:18 +0000

    Section 2 - GraphQL Basics: Schemas and Queries - 2.7. Operation Arguments.

---
+++ Section 2 - GraphQL Basics - Schemas and Queries/Section 2.7 - Operation Arguments/graphql-basics/src/challenge.js
@@ -0,0 +1,3 @@
+// 1. Create an "add" query that returns a float
+// 2. Setup "add" to take two arguments (a, b) which are required floats
+// 3. Have the resolver send back the sum of the two arguments

fd2f25c6758f9c8aa8a5a17e1b58be0e6dc9a6e8
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-01-19 17:14:08 +0000

    Section 2 - GraphQL Basics: Schemas and Queries - 2.11. Comment Challenge: Part I.

---
+++ Section 2 - GraphQL Basics - Schemas and Queries/Section 2.11 - Comment Challenge/graphql-basics/src/challenge.js
@@ -0,0 +1,5 @@
+// Part I:
+// 1. Setup a "Comment" custom type with id and text fields. Both non-nullable
+// 2. Setup a "comments" array with four comments
+// 3. Setup a "comments" query with a resolver that returns all the comments
+// 4. Run a query to get all four comments with both id and text fields in GraphQL Playground (run the terminal command npm run start and in the browser go to localhost:4000)

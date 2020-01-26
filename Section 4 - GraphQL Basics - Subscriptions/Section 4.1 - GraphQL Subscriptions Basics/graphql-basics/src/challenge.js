fe1861ebc07903797d823a1a91f493ab51150889
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-01-26 21:26:48 +0000

    Section 4 - GraphQL Basics: Subscriptions - 4.1. GraphQL Subscription Basics.

---
+++ Section 4 - GraphQL Basics - Subscriptions/Section 4.1 - GraphQL Subscriptions Basics/graphql-basics/src/challenge.js
@@ -0,0 +1,15 @@
+// Goal: Setup a mutation for updating a post and comments
+// 1. Define mutation
+//    - Add id/data for the operation arguments. Setup data to support title, body and published
+//    - Return the updated post
+// 2. Create resolver method
+//    - Verify post exists, else throw error
+//    - Update post properties one at a time
+// 3. Test by updating all properties for a given post in GraphQL Playground (run the terminal command npm run start and in the browser go to localhost:4000)
+// 4. Define mutation
+//    - Add id/data for the operation arguments. Setup data to support text
+//    - Return the updated comment
+// 5. Create resolver method
+//    - Verify comment exists, else throw error
+//    - Update comment properties one at a time
+// 6. Test by updating all properties for a given comment in GraphQL Playground (run the terminal command npm run start and in the browser go to localhost:4000)

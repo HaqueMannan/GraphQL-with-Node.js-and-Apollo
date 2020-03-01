ba7e8403083d08357daec51a52401023c15649d0
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-03-01 15:07:08 +0000

    Section 6 - Authentication with GraphQL - 6.4. Closing Prisma to the Outside World.

---
+++ Section 6 - Authentication with GraphQL/6.4 - Closing Prisma to the Outside World/graphql-prisma/src/resolvers/Mutation.js
@@ -0,0 +1,99 @@
+import uuidv4 from 'uuid/v4';
+
+// All resolvers are passed four arguments in a specific order of: parent, args, ctx and info
+// ctx can be destructed to grab the db object instead of writing ctx.db
+const Mutation = {
+   async createUser(parent, args, { prisma }, info) {
+      const emailTaken = await prisma.exists.User({ email: args.data.email });
+
+      if(emailTaken) {
+         throw new Error('Email taken.');
+      };
+
+      return prisma.mutation.createUser({ data: args.data }, info);
+   },
+   async deleteUser(parent, args, { prisma }, info) {
+      const userExists = await prisma.exists.User({ id: args.id });
+
+      if(!userExists) {
+         throw new Error('User not found.');
+      };
+
+      return prisma.mutation.deleteUser({ 
+         where: {
+            id: args.id
+         }
+      }, info);
+   },
+   async updateUser(parent, args, { prisma }, info) {
+      return prisma.mutation.updateUser({
+         where: {
+            id: args.id
+         },
+         data: args.data
+      }, info);
+   },
+   createPost(parent, args, { prisma }, info) {
+      return prisma.mutation.createPost({
+         data: {
+            title: args.data.title,
+            body: args.data.body,
+            published: args.data.published,
+            author: {
+               connect: {
+                  id: args.data.author
+               }
+            }
+         }
+      }, info);
+   },
+   deletePost(parent, args, { prisma }, info) {
+      return prisma.mutation.deletePost({
+         where: {
+            id: args.id
+         }
+      }, info);
+   },
+   updatePost(parent, args, { prisma }, info) {
+      return prisma.mutation.updatePost({
+         where: {
+            id: args.id
+         },
+         data: args.data
+      }, info);
+   },
+   createComment(parent, args, { prisma }, info) {
+      return prisma.mutation.createComment({
+         data: {
+            text: args.data.text,
+            author: {
+               connect: {
+                  id: args.data.author
+               }
+            },
+            post: {
+               connect: {
+                  id: args.data.post
+               }
+            }
+         }
+      }, info);
+   },
+   deleteComment(parent, args, { prisma }, info) {
+      return prisma.mutation.deleteComment({
+         where: {
+            id: args.id
+         }
+      }, info);
+   },
+   updateComment(parent, args, { prisma }, info) {
+      return prisma.mutation.updateComment({
+         where: {
+            id: args.id
+         },
+         data: args.data
+      }, info);
+   }
+};
+
+export { Mutation as default };

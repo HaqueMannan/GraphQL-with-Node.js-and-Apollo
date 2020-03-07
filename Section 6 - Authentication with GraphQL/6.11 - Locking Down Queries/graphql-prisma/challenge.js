5e4276d5962701887f1b4a7df2a6c3369617933b
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-03-07 21:52:45 +0000

    Section 6 - Authentication with GraphQL - 6.11. Locking Down Queries.

---
+++ Section 6 - Authentication with GraphQL/6.11 - Locking Down Queries/graphql-prisma/challenge.js
@@ -0,0 +1,13 @@
+// Goal: Lock down me query
+// 1. Require authentication
+// 2. Use the correct prisma query to get/return user by their id
+// 3. Test the me query in GraphQL Playground (run the terminal command npm run start and in the browser go to localhost:4000)
+
+// Goal: Create a query for accessing your posts (myPosts)
+// 1. Define the query in schema.graphql
+//    - Accept optional query string. Return array of posts
+// 2. Require authentication
+// 3. Setup opArgs to just fetch posts by the authenticated user
+// 4. Setup support for query argument list with posts
+// 5. Use the correct prisma query to get/return data
+// 6. Test the me query in GraphQL Playground (run the terminal command npm run start and in the browser go to localhost:4000)

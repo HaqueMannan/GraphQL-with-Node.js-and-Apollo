6616c49eaf835125396013d09e7758de2fdb7611
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-03-08 18:47:10 +0000

    Section 6 - Authentication with GraphQL - 6.15. Token Expiration.

---
+++ Section 6 - Authentication with GraphQL/6.15 - Token Expiration/graphql-prisma/challenge.js
@@ -0,0 +1,4 @@
+// Goal: Create a utility function for generating JWT
+// 1. Create a new generateToken.js file in the utils directory
+// 2. Create a function that takes the user id and returns the token
+// 4. Replace the two jwt.sign calls with the calls to generateToken instead

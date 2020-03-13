2a4fd2719a8fd201edb56c0ab5d9a4b3d4860d88
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-03-13 20:45:41 +0000

    Section 7 - Pagination and Sorting with GraphQL - 7.1. Pagination.

---
+++ Section 7 - Pagination and Sorting with GraphQL/7.1 - Pagination/graphql-prisma/challenge.js
@@ -0,0 +1,4 @@
+// Goal: Add pagination for post query
+// 1. Add necessary arguments to posts query
+// 2. Pass arguments through to Prisma in the posts resolver
+// 3. Test the posts query by using the fist and skip arguments in GraphQL Playground (run the terminal command npm run start and in the browser go to localhost:4000)

cc0547a9e8f9660cb2c1d7573fb3d7b414795be2
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-03-13 22:06:10 +0000

    Section 7 - Pagination and Sorting with GraphQL - 7.3. Working with createdAt and updatedAt.

---
+++ Section 7 - Pagination and Sorting with GraphQL/7.3 - Working with createdAt and updatedAt/graphql-prisma/src/utils/getUserId.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const getUserId = (request, requireAuth = true) => {
+   // header Authentication token = (dynamically set using ternary operator) HTTP : Websocket
+   const header = request.request ? request.request.headers.authorization : request.connection.context.Authorization
+
+   if(header) {
+      const token = header.replace('Bearer ', '');
+      const decoded = jwt.verify(token, 'nodesecret');
+      return decoded.userId;
+   };
+
+   if (requireAuth) {
+      throw new Error('Authentication required');
+   }
+
+   // implicitly returns undefined if there is not header and requireAuth is false
+   // We want to explicitly return null instead of undefined
+   return null;
+};
+
+export { getUserId as default };

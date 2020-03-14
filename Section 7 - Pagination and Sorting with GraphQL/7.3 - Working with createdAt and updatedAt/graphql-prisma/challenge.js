dbe675bfc584821789affdd7885dd1dfff7fa9b8
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-03-14 17:04:09 +0000

    Section 7 - Pagination and Sorting with GraphQL - 7.4. Sorting Data.

--- Section 7 - Pagination and Sorting with GraphQL/7.3 - Working with createdAt and updatedAt/graphql-prisma/challenge.js
+++
@@ -1,4 +0,0 @@
-// Goal: Add pagination for myPosts and comments query
-// 1. Add three necessary arguments to the query definitions
-// 2. Pass arguments through to Prisma in the myPosts and comments resolver
-// 3. Test the myPosts and comments query by using the fist, skip and after arguments in GraphQL Playground (run the terminal command npm run start and in the browser go to localhost:4000)
---
+++ Section 7 - Pagination and Sorting with GraphQL/7.4 - Sorting Data/graphql-prisma/challenge.js
@@ -0,0 +1,4 @@
+// Goal: Setup sorting for posts, myPosts and comment
+// 1. Setup the orderBy argument with the correct imported enum
+// 2. Update resolvers to pass the argument through to prisma
+// 5. Test Ascending and Descending order sort on the posts, myPosts and comments queries in GraphQL Playground (run the terminal command npm run start and in the browser go to localhost:4000)

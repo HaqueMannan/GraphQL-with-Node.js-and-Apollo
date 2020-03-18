424be5db09a3f2ffb2bfd7a5dc57f3687ef3bff5
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-03-18 21:16:22 +0000

    Section 9 - Apollo Client and Testing GraphQL - 9.1. Setting up a Test Environment & Installing Jest.

---
+++ Section 9 - Apollo Client and Testing GraphQL/9.1 - Setting up a Test Environment & Installing Jest/graphql-prisma/src/resolvers/Query.js
@@ -0,0 +1,113 @@
+import getUserId from '../utils/getUserId';
+
+// All resolvers are passed four arguments in a specific order of: parent, args, ctx and info
+// ctx can be destructed to grab the db object instead of writing ctx.db
+const Query = {
+   users(parent, args, { prisma }, info) {
+      const opArgs = {
+         first: args.first,
+         skip: args.skip,
+         after: args.after,
+         orderBy: args.orderBy
+      };
+
+      if(args.query) {
+         opArgs.where = {
+            OR: [{
+               name_contains: args.query
+            }]
+         };
+      };
+
+      return prisma.query.users(opArgs, info);
+   },
+   myPosts(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request);
+
+      const opArgs = {
+         first: args.first,
+         skip: args.skip,
+         after: args.after,
+         orderBy: args.orderBy,
+         where: {
+            author: {
+               id: userId
+            }
+         }
+      };
+
+      if(args.query) {
+         opArgs.where.OR = [{
+            title_contains: args.query
+         }, {
+            body_contains: args.query
+         }]
+      };
+
+      return prisma.query.posts(opArgs, info);
+   },
+   posts(parent, args, { prisma }, info) {
+      const opArgs = {
+         first: args.first,
+         skip: args.skip,
+         after: args.after,
+         orderBy: args.orderBy,
+         where: {
+            published: true
+         }
+      };
+
+      if(args.query) {
+         opArgs.where.OR = [{
+            title_contains: args.query
+         }, {
+            body_contains: args.query
+         }]
+      };
+
+      return prisma.query.posts(opArgs, info);
+   },
+   comments(parent, args, { prisma }, info) {
+      const opArgs = {
+         first: args.first,
+         skip: args.skip,
+         after: args.after,
+         orderBy: args.orderBy
+      };
+
+      return prisma.query.comments(opArgs, info);
+   },
+   me(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request);
+
+      return prisma.query.user({
+         where: {
+            id: userId
+         }
+      })
+   },
+   async post(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request, false);
+
+      const posts = await prisma.query.posts({
+         where: {
+            id: args.id,
+            OR: [{
+               published: true
+            }, {
+               author: {
+                  id: userId
+               }
+            }]
+         }
+      }, info);
+
+      if(posts.length === 0) {
+         throw new Error('Post not found');
+      };
+
+      return posts[0];
+   }
+};
+
+export { Query as default };
